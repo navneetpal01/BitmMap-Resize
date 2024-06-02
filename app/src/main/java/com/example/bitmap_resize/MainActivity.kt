7b14050b77a46893d7c154f318576b53c3be291c
navneet <navneetpalgithub@gmail.com>
2024-06-02 09:51:04 +0530

    Mars

---
+++ app/src/main/java/com/example/bitmap_resize/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.bitmap_resize
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.bitmap_resize.ui.theme.BitmapResizeTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            BitmapResizeTheme {
+
+            }
+        }
+    }
+
+}
