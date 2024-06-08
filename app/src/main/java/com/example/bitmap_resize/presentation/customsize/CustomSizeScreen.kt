4d7099fa2c67aa1db995a28de4b57a3a041e1ad5
navneet <navneetpalgithub@gmail.com>
2024-06-08 18:19:09 +0530

    Mars

--- app/src/main/java/com/example/bitmap_resize/presentation/customsize/CustomSizeScreen.kt
+++ app/src/main/java/com/example/bitmap_resize/presentation/customsize/CustomSizeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.bitmap_resize.presentation.customsize
 
+import android.annotation.SuppressLint
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
@@ -16,6 +18,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -27,24 +31,32 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 
+@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomSizeScreen(
@@ -53,6 +65,7 @@ fun CustomSizeScreen(
 ) {
 
     val bitmapState = viewModel.bitmap.collectAsState().value
+    val context = LocalContext.current
 
     val imagePicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
@@ -119,6 +132,7 @@ fun CustomSizeScreen(
                     onClick = {
                         if (bitmapState != null) {
                             viewModel.onEvent(CustomSizeEvent.OnConvert)
+                            Toast.makeText(context,"Converted Successfully",Toast.LENGTH_LONG).show()
                         }
                     },
                     colors = ButtonDefaults.buttonColors(
@@ -143,6 +157,7 @@ fun CustomSizeScreen(
                     onClick = {
                         if (bitmapState != null) {
                             viewModel.onEvent(CustomSizeEvent.OnSave)
+                            Toast.makeText(context,"Saved Successfully",Toast.LENGTH_LONG).show()
                         }
                     },
                     colors = ButtonDefaults.buttonColors(
@@ -217,20 +232,22 @@ fun CustomSizeScreen(
                     )
                 )
                 Spacer(modifier = Modifier.height(12.dp))
-                OutlinedTextField(
+                TextField(
                     value = viewModel.state.height.toString(),
                     onValueChange = {
                         viewModel.state = viewModel.state.copy(height = it.toInt())
                     },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 10.dp, vertical = 5.dp)
                 )
-                OutlinedTextField(
+                TextField(
                     value = viewModel.state.width.toString(),
                     onValueChange = {
                         viewModel.state = viewModel.state.copy(width = it.toInt())
                     },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 10.dp, vertical = 5.dp)
